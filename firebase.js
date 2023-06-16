187b7d456b17a96930e8598d599e42bb43f7f268
Akshat Mehta <56683451+1akmaku@users.noreply.github.com>
2023-06-16 16:29:29 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+//Passkeys
+import firebase from 'firebase'
+import "firebase/auth"
+export const auth=firebase.initializeApp ({
+    apiKey: "AIzaSyA9ENoDE_n0ZFmvbOJsjQvLxDeUCLFpXoU",
+    authDomain: "desi-messanger.firebaseapp.com",
+    projectId: "desi-messanger",
+    storageBucket: "desi-messanger.appspot.com",
+    messagingSenderId: "417324811011",
+    appId: "1:417324811011:web:413296d478169846da5f36"
+  } ).auth()
